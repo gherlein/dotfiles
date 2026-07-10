86a69e4912ff7f84b6382d703636578361f26181
Greg Herlein <gherlein@herlein.com>
2026-07-10 06:32:41 -0700

    interim commit

---
+++ emacs/.emacs.d/straight/build-cache.el
@@ -0,0 +1,14 @@
+
+:tanat
+
+"30.2"
+
+#s(hash-table test equal data ("straight" ("2026-07-06 08:31:21" nil (:type git :host github :repo "radian-software/straight.el" :branch "master" :package "straight" :local-repo "straight.el"))))
+
+#s(hash-table test equal)
+
+#s(hash-table test eq)
+
+("straight")
+
+t
